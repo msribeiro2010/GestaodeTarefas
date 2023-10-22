94c04cb8c02757663ab94ac6f84e08cfda138f84
msribeiro2010 <msribeiro2010@icloud.com>
2023-10-22 19:09:38 -0300

    Versao2

--- script.js
+++ script.js
@@ -1,101 +1,133 @@
+// Inicialização do IndexedDB
+let db;
 let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 let deleteAllButton = document.querySelector("#delete-all");
 let filterButtons = document.querySelectorAll(".filter-button");
+let tarefas = [];
 
-let tarefas = JSON.parse(localStorage.getItem("@listTarefas")) || [];
+const request = indexedDB.open("tarefasDB", 1);
 
-function renderTarefas(tasksToRender = tarefas) {
-  listElement.innerHTML = "";
-
-  tasksToRender.map((tarefa) => {
-    let itemElement = document.createElement("li");
-    //let itemText = document.createTextNode(tarefa.text + " (" +"" + tarefa.timestamp + ")");
-    let itemText = document.createTextNode("" + tarefa.timestamp + " - " + tarefa.text);
-
-    let linkElement = document.createElement("a");
-    linkElement.setAttribute("href", "#");
-
-    let linkText = document.createTextNode("Excluir");
-    linkElement.appendChild(linkText);
-
-    linkElement.setAttribute("onclick", "deletarTarefa(" + tarefas.indexOf(tarefa) + ")");
-
-    itemElement.addEventListener("click", () => toggleCompleted(tarefas.indexOf(tarefa)));
-    if (tarefa.completed) {
-      itemElement.classList.add("completed");
+request.onupgradeneeded = function (event) {
+    db = event.target.result;
+    if (!db.objectStoreNames.contains("tarefas")) {
+        db.createObjectStore("tarefas", { autoIncrement: true });
     }
+};
 
-    itemElement.appendChild(itemText);
-    itemElement.appendChild(linkElement);
-    listElement.appendChild(itemElement);
-  });
+request.onsuccess = function (event) {
+    db = event.target.result;
+    carregarTarefas();
+};
+
+request.onerror = function (event) {
+    console.log("Erro ao abrir DB", event);
+};
+
+function carregarTarefas() {
+    tarefas = [];
+    const transaction = db.transaction(["tarefas"], "readonly");
+    const store = transaction.objectStore("tarefas");
+
+    store.openCursor().onsuccess = function(event) {
+        const cursor = event.target.result;
+        if (cursor) {
+            tarefas.push(cursor.value);
+            cursor.continue();
+        } else {
+            renderTarefas();
+        }
+    };
 }
 
-renderTarefas();
+function renderTarefas(tasksToRender = tarefas) {
+    listElement.innerHTML = "";
+
+    tasksToRender.map((tarefa) => {
+        let itemElement = document.createElement("li");
+        let itemText = document.createTextNode("" + tarefa.timestamp + " - " + tarefa.text);
+
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        let linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+        linkElement.setAttribute("onclick", "deletarTarefa(" + tarefas.indexOf(tarefa) + ")");
+        
+        itemElement.addEventListener("click", () => toggleCompleted(tarefas.indexOf(tarefa)));
+
+        if (tarefa.completed) {
+            itemElement.classList.add("completed");
+        }
+
+        itemElement.appendChild(itemText);
+        itemElement.appendChild(linkElement);
+        listElement.appendChild(itemElement);
+    });
+}
 
 function adicionarTarefa() {
-  if (inputElement.value == "") {
-    alert("Digite uma tarefa");
-    return false;
-  } else {
-    console.log(inputElement.value);
-  }
-  let tarefaText = inputElement.value;
-  let dateFormatter = new Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit'
-});
-let timestamp = dateFormatter.format(new Date());
+    if (inputElement.value === "") {
+        alert("Digite uma tarefa");
+        return;
+    }
+    let tarefaText = inputElement.value;
+    let dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    let timestamp = dateFormatter.format(new Date());
 
-  tarefas.push({ text: tarefaText, completed: false, timestamp: timestamp });
-  inputElement.value = "";
-  listElement.innerHTML = "";
-
-  renderTarefas();
-  salvarTarefas();
+    tarefas.push({ text: tarefaText, completed: false, timestamp: timestamp });
+    inputElement.value = "";
+    renderTarefas();
+    salvarTarefas();
 }
 
 buttonElement.addEventListener("click", adicionarTarefa);
 
 function deletarTarefa(index) {
-  tarefas.splice(index, 1);
-  renderTarefas();
-  salvarTarefas();
+    tarefas.splice(index, 1);
+    renderTarefas();
+    salvarTarefas();
 }
 
 function salvarTarefas() {
-  localStorage.setItem("@listTarefas", JSON.stringify(tarefas));
+    const transaction = db.transaction(["tarefas"], "readwrite");
+    const store = transaction.objectStore("tarefas");
+    
+    store.clear();
+    tarefas.forEach(tarefa => {
+        store.add(tarefa);
+    });
 }
 
 function deleteAllTasks() {
-  tarefas = [];
-  renderTarefas();
-  salvarTarefas();
+    tarefas = [];
+    renderTarefas();
+    salvarTarefas();
 }
 
 deleteAllButton.addEventListener("click", deleteAllTasks);
 
 function filterTasks(filter) {
-  let filteredTasks = tarefas.filter((tarefa) => {
-    if (filter === "all") return true;
-    if (filter === "active") return !tarefa.completed;
-    if (filter === "completed") return tarefa.completed;
-  });
-
-  renderTarefas(filteredTasks);
+    let filteredTasks = tarefas.filter((tarefa) => {
+        if (filter === "all") return true;
+        if (filter === "active") return !tarefa.completed;
+        if (filter === "completed") return tarefa.completed;
+    });
+    renderTarefas(filteredTasks);
 }
 
-filterButtons.forEach((button) =>
-  button.addEventListener("click", () => filterTasks(button.dataset.filter))
+filterButtons.forEach((button) => 
+    button.addEventListener("click", () => filterTasks(button.dataset.filter))
 );
 
 function toggleCompleted(index) {
-  tarefas[index].completed = !tarefas[index].completed;
-  renderTarefas();
-  salvarTarefas();
+    tarefas[index].completed = !tarefas[index].completed;
+    renderTarefas();
+    salvarTarefas();
 }
